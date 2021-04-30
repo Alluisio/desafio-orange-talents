b7a39ae70fc30175a2cd8b351419b9287d1b4a96
Alluisio Silva <alluisio.silva@ipdec.org>
2021-04-30 19:34:41 -0400

    Adição de tabelas via liquibase
    1. Foi adicionado a tabela usuario;
    2. Foi adicionado a tabela endereço;

---
+++ src/test/java/orange/desafioorange/DesafioOrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.desafioorange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioOrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
